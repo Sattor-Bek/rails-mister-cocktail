e9acce45a6937479f54da2956360451e0caff0d9
Satoru Nishizawa <satoru.nh@gmail.com>
2019-11-07 19:28:31 +0900

    gotta make css and html

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,28 +1,22 @@
 class DosesController < ApplicationController
   before_action :set_dose, only: [:show, :update, :edit, :destroy]
-
+  before_action :set_cocktail, only: [:create, :new]
   def index
     @doses = Dose.all
   end
 
   def new
-    @ingredient = Ingredient.find(params[:ingredient_id])
-    @dose.ingredient = @ingredient
-    puts @ingredient.name
-    puts @cocktail.name
-    @cocktail = Cocktail.find(params[:cocktail_id])
+    # @ingredient = Ingredient.find(params[:ingredient_id])
+    @dose = Dose.new
     @dose.cocktail = @cocktail
-    @dose = Dose.new(dose_params)
   end
 
   def create
-    @ingredient = Ingredient.find(params[:ingredient_id])
-    @dose.ingredient = @ingredient
-    @cocktail = Cocktail.find(params[:cocktail_id])
-    @dose.cocktail = @cocktail
+    # @ingredient = Ingredient.find(params[:ingredient_id])
     @dose = Dose.new(dose_params)
+    @dose.cocktail = @cocktail
     if @dose.save
-      redirect_to dose_path(@dose)
+      redirect_to cocktail_path(@cocktail[:id])
     else
       render :new
     end
@@ -35,7 +29,7 @@ class DosesController < ApplicationController
   end
 
   def update
-    @dose.update(dose_params)
+    # @dose.update(dose_params)
     # @ingredient = Ingredient.find(params[:ingredient_id])
     # @dose.ingredient = @ingredient
     redirect_to dose_path(@dose)
@@ -43,13 +37,17 @@ class DosesController < ApplicationController
 
   def destroy
     @dose.destroy
-    redirect_to doses_path
+    redirect_to cocktails_path
   end
 
     private
   def dose_params
-    params.require(:dose).permit(:cocktail_id, :ingredient_id)
+    params.require(:dose).permit(:ingredient_id, :description)
   end
+  def set_cocktail
+    @cocktail = Cocktail.find(params[:cocktail_id])
+  end
+
 
   def set_dose
     @dose = Dose.find(params[:id])
